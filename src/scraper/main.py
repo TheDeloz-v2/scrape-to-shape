0a7c183f250db1669955caeb78a7ac3a1c8d0f18
Francisco Castillo <castillocerna@gmail.com>
2025-10-06 12:48:52 -0600

    Restructure project: move main and scrapper modules
    Moved main.py and scrapper.py into the new src/scraper/ directory to improve project organization. Updated DEFAULT_OUT_DIR path in main.py to reflect the new directory structure.

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -9,7 +9,7 @@ LINKS: List[Tuple[str, str]] = [
     ("club_montania", "https://www.google.com/maps/place/Club+Campestre+La+monta%C3%B1a/@14.6349945,-90.6511765,13z/data=!4m10!1m2!2m1!1sclub!3m6!1s0x85899fe4af26f5ef:0xaa5237c68a36bc1a!8m2!3d14.671049!4d-90.59737!15sCgRjbHViWgYiBGNsdWKSAQxjb3VudHJ5X2NsdWKaASNDaFpEU1VoTk1HOW5TMFZKUTBGblNVTXlOblpEUzA5bkVBRaoBNhABKggiBGNsdWIoDjIeEAEiGhmb-Djzzmzp2o8QX95WJZwjDT6Md0iPrXCKMggQAiIEY2x1YuABAPoBBAgAECk!16s%2Fg%2F1tp08fqj?entry=ttu&g_ep=EgoyMDI1MTAwMS4wIKXMDSoASAFQAw%3D%3D"),
     ("club_americano", "https://www.google.com/maps/place/Club+Americano/@14.6063905,-90.5754284,13z/data=!4m10!1m2!2m1!1sclub!3m6!1s0x8589a3b26f62b751:0x79b676724cd180fb!8m2!3d14.6063905!4d-90.4992107!15sCgRjbHViWgYiBGNsdWKSAQtzcG9ydHNfY2x1YpoBJENoZERTVWhOTUc5blMwVkpRMEZuU1VSMWJtVkVWR2gzUlJBQqoBNhABKggiBGNsdWIoDjIeEAEiGhmb-Djzzmzp2o8QX95WJZwjDT6Md0iPrXCKMggQAiIEY2x1YuABAPoBBAgAECI!16s%2Fg%2F1tjmd_0q?entry=ttu&g_ep=EgoyMDI1MTAwMS4wIKXMDSoASAFQAw%3D%3D")
 ]
-DEFAULT_OUT_DIR = "../data/raw"
+DEFAULT_OUT_DIR = "../../data/raw"
 COMBINED_FILENAME = "google_maps_all_reviews.csv"
 
 async def process_all(pairs, out_dir=DEFAULT_OUT_DIR, com_file=COMBINED_FILENAME, headless=False, sequential=True):
